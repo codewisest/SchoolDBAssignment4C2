c7ed92c732a393466168a9046170e90e1c77e6c5
Chijioke <wisestcodes@gmail.com>
2023-04-21 20:13:55 -0400

    Add project files.

---
+++ SchoolDBAssignment4C2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SchoolDBAssignment4C2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SchoolDBAssignment4C2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SchoolDBAssignment4C2.WebApiApplication" Language="C#" %>
